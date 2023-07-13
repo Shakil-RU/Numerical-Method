c4a244b35c3b023e6e43c311c83caa9521df4f7c
Shakil hossan <shakilnirob111@gmail.com>
2023-07-13 22:02:40 +0600

    Update Gauss-Elimination-Method.cpp

--- Gauss-Elimination-Method.cpp
+++ Gauss-Elimination-Method.cpp
@@ -1,45 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class SimpsonRule
-{
+class GaussElimination {
 public:
     int n;
-    double ans = 0, y0, yn, h;
-    double x[10], y[10];
-    SimpsonRule()
-    {
-        cin >> n;
-        for (int i = 0; i < n; i++)
-        {
-            cin >> x[i] >> y[i];
+    double augmented_matrix[10][10], x[10];
+
+    GaussElimination() {
+        cin>>n;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j <= n; j++) {
+                cin>>augmented_matrix[i][j];
+            }
         }
-        y0 = y[0];
-        yn = y[n - 1];
-        h = x[1] - x[0];
     }
-    void solve()
-    {
-        ans = y0 + yn;
-        for (int i = 1; i < n - 1; i++)
-        {
-            if (i % 2 == 1)
-            {
-                ans = ans + 4 * y[i];
+
+    void solve() {
+
+        // Gauss Elimination Formmula
+        for(int i = 0; i < n - 1; i++) {
+            if(augmented_matrix[i][i] == 0.0) {
+                cout<<"Can't be solved using Gauss Elimination Method\n";
+                exit(0);
             }
-            else
-            {
-                ans = ans + 2 * y[i];
+            for(int j = i + 1; j < n; j++) {
+                double ratio = augmented_matrix[j][i] / augmented_matrix[i][i];
+                for(int k = 0; k <= n; k++) {
+                    augmented_matrix[j][k] -= augmented_matrix[i][k] * ratio;
+                }
             }
         }
-        ans = (ans * h) / 3;
-        cout << "Answer = " << ans << endl;
+
+        // Back Substitution
+        x[n-1] = augmented_matrix[n-1][n] / augmented_matrix[n-1][n-1];
+        for(int i = n - 2; i >= 0; i--) {
+            x[i] = augmented_matrix[i][n];
+            for(int j = i + 1; j < n; j++) {
+                x[i] -= augmented_matrix[i][j] * x[j];
+            }
+            x[i] = x[i] / augmented_matrix[i][i];
+        }
+
+        // Display the values of x
+        for(int i = 0; i < n; i++) {
+            cout<<"x"<<i<<" = "<<x[i];
+            cout<<"\n";
+        }
     }
 };
 
-int main()
+int main(void)
 {
-    SimpsonRule s = SimpsonRule();
-    s.solve();
-    return 0;
+    GaussElimination g = GaussElimination();
+    g.solve();
 }
